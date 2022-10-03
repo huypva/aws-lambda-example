d8327558397d8a2aa20887aab529c71a0642237c
huypva <huypva@vng.com.vn>
2022-10-03 17:28:47 +0700

    Update readme

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,14 +1,15 @@
-cd hello-world
 printf '\n\nBuilding the Java Lambda Function!\n\n'
+cd hello-world
 mvn clean verify
 if [ $? -ne 0 ]; then
   printf '\n\nJava application build failed! No new Lambda Function will be deployed!!!i\n'
   exit -1
 fi
-
 cd ..
-printf '\n\nStarting the Terraforming!\n\n'
+
+printf '\n\nStarting the Terraform!\n\n'
 cd terraform
 terraform init
 terraform plan -out=plan.out
 terraform apply plan.out
+cd ..
